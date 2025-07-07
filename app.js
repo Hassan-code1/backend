1f4c588daaea32dd27a6fc0f7ecef370fe153043
Hassan-code1 <2024kuec2011@iiitkota.ac.in>
2025-07-07 15:38:54 +0530

    7june

--- app.js
+++ app.js
@@ -3,19 +3,53 @@ const app = express()
 const port = 3000
 const path = require('path');
 // const userModel = require("./usermodel");
-const userModel = require("./models/user")
+const userModel = require("./models/user");
+const cookieParser = require('cookie-parser');
+const { log } = require('console');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
+    //encryption at password saving
+    // bcrypt.genSalt(10, function(err, salt){
+    //     bcrypt.hash("Password", salt, function(err, hash){
+    //         console.log(hash);
+    //     });
+    // });
+    
+    //checking entered password is same as saved one or not
+    // bcrypt.compare("Password", "$2b$10$T8Q8oPE0V9YwigcNL2eKXeKdSzi3sPMY..tCS/OFvHYH9PDnyjKxC", function(err, result){
+    //     console.log(result);
+    // });
+
+    //token creation for cookie to send to browser
+    // let token = jwt.sign({email:"hassan@gmail.com"}, "secret");
+    // res.cookie("token", token);
+    // console.log(token);
+    
+    //sending cookies to browser
+    // res.cookie("name", "Hassan");
+    // (req.cookies)
+    // res.send("done");
+    // console.log(req.cookies);
+
+    //removing token from browser
+    // res.cookie("token", "");
     res.render('index')
 })
 
 app.get("/read", async (req, res) => {
     let users = await userModel.find()
     res.render('read', {users})
+    // console.log(req.cookies.token);
+    let data = jwt.verify(req.cookies.token, "secret");
+    console.log(data);
 })
 
 app.post("/create", async (req, res) => {
